0aa5da60172d656c496cb4777efc99a632480b23
Terry Moreland <tsmoreland@live.com>
2022-11-03 23:54:53 +0000

    add initial implementation calling pluralsight author api

--- src/main/java/ca/tsmoreland/courseinfo/cli/service/CourseRetrievalService.java
+++ src/main/java/ca/tsmoreland/courseinfo/cli/service/CourseRetrievalService.java
@@ -1,4 +1,27 @@
 package ca.tsmoreland.courseinfo.cli.service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CourseRetrievalService {
+
+    private static final String PLURALSIGHT_API_URI = "https://app.pluralsight.com/profile/data/author/%s/all-content";
+    private static final HttpClient CLIENT = HttpClient.newHttpClient();
+
+    public String getCoursesFor(String authorId) {
+        HttpRequest request = HttpRequest.
+            newBuilder(URI.create(PLURALSIGHT_API_URI.formatted(authorId))).
+            GET().
+            build();
+
+        try {
+            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Error occurred calling API", e);
+        }
+    }
 }
