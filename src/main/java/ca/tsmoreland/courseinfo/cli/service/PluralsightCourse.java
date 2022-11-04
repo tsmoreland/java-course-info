e5a39b0b9d29fa17571ff86bb4dfb61d87b7af95
Terry Moreland <tsmoreland@live.com>
2022-11-04 00:55:02 +0000

    add method to PluralsightCourse to parse duration string to Optional of Duration - empty if unable to parse

--- src/main/java/ca/tsmoreland/courseinfo/cli/service/PluralsightCourse.java
+++ src/main/java/ca/tsmoreland/courseinfo/cli/service/PluralsightCourse.java
@@ -2,6 +2,10 @@ package ca.tsmoreland.courseinfo.cli.service;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.Optional;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record PluralsightCourse(
     String id,
@@ -10,4 +14,14 @@ public record PluralsightCourse(
     String contentUrl,
     String level,
     boolean isRetired) {
+
+    public Optional<Duration> parseDuration() {
+        try {
+
+            return Optional.of(Duration.between(LocalTime.MIN, LocalTime.parse(duration())));
+        } catch (Exception e) {
+
+            return Optional.empty();
+        }
+    }
 }
