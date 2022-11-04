b1ad6142a285a7e21a18d0c475cb898728e8a1a2
Terry Moreland <tsmoreland@live.com>
2022-11-04 00:28:45 +0000

    update course retrieval service to use jackson to convert json to Pluralsight course

--- src/main/java/ca/tsmoreland/courseinfo/cli/service/CourseRetrievalService.java
+++ src/main/java/ca/tsmoreland/courseinfo/cli/service/CourseRetrievalService.java
@@ -1,10 +1,15 @@
 package ca.tsmoreland.courseinfo.cli.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class CourseRetrievalService {
 
@@ -14,7 +19,9 @@ public class CourseRetrievalService {
         followRedirects(HttpClient.Redirect.ALWAYS).
         build();
 
-    public String getCoursesFor(String authorId) {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public List<PluralsightCourse> getCoursesFor(String authorId) {
         HttpRequest request = HttpRequest.
             newBuilder(URI.create(PLURALSIGHT_API_URI.formatted(authorId))).
             GET().
@@ -24,12 +31,19 @@ public class CourseRetrievalService {
             HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
 
             return switch(response.statusCode()) {
-                case 200 -> response.body();
-                case 404 -> "";
+                case 200 -> toPluralsightCourses(response);
+                case 404 -> List.of();
                 default -> throw new RuntimeException("API Call failed with " + response.statusCode());
             };
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Error occurred calling API", e);
         }
+    }
+
+    private static List<PluralsightCourse> toPluralsightCourses(HttpResponse<String> response) throws JsonProcessingException {
+        JavaType type = OBJECT_MAPPER.
+            getTypeFactory().
+            constructCollectionType(List.class, PluralsightCourse.class);
+        return OBJECT_MAPPER.readValue(response.body(), type);
     }
 }
