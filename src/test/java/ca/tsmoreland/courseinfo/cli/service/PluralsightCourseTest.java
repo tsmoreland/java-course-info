bb04bdb3a59fde307c47caeee2d95360d94dc956
Terry Moreland <tsmoreland@live.com>
2022-11-04 01:04:05 +0000

    move to parameterized tests and add surefire plugin so maven sees the tests

--- src/test/java/ca/tsmoreland/courseinfo/cli/service/PluralsightCourseTest.java
+++ src/test/java/ca/tsmoreland/courseinfo/cli/service/PluralsightCourseTest.java
@@ -1,20 +1,25 @@
 package ca.tsmoreland.courseinfo.cli.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.Duration;
 import java.util.Optional;
 
 class PluralsightCourseTest {
 
-    @Test
-    void parseDuration() {
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+        00:30:00, 1800
+        00:00:00, 0
+        """)
+    void parseDurationShouldReturnDurationWhenDurationStringIsValid(String sourceDuration, long expectedSeconds) {
 
-        var course = new PluralsightCourse("", "title", "00:30:00", "https://example.com", "Beginner", false);
+        var course = new PluralsightCourse("4fc31c8b-97c7-4bb9-a8be-639d8992146b", "title", sourceDuration, "https://example.com", "Beginner", false);
 
         Optional<Duration> actual = course.parseDuration();
-        Duration expected = Duration.ofMinutes(30);
+        Duration expected = Duration.ofSeconds(expectedSeconds);
 
         assertTrue(actual.isPresent());
         assertEquals(actual.get(), expected);
