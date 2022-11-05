0aa19be478196505dc7348bebaaa6c24a4539e50
Terry Moreland <terry.s.moreland@gmail.com>
2022-11-05 14:55:51 +0000

    add course storage service

---
+++ course-info-cli/src/main/java/ca/tsmoreland/courseinfo/cli/service/CourseStorageService.java
@@ -0,0 +1,31 @@
+package ca.tsmoreland.courseinfo.cli.service;
+
+import ca.tsmoreland.courseinfo.repository.CourseRepository;
+
+import java.time.Duration;
+import java.util.List;
+
+public class CourseStorageService {
+    private static final String PS_URL = "https://app.pluralsight.com%s";
+    private final CourseRepository repository;
+
+    public CourseStorageService(CourseRepository repository) {
+        this.repository = repository;
+    }
+
+    public void storePluralsightCourses(List<PluralsightCourse> courses) {
+        for (PluralsightCourse course: courses) {
+            var entity = toEntity(course);
+            repository.addAndSaveCourse(entity);
+        }
+    }
+
+    private ca.tsmoreland.courseinfo.domain.PluralsightCourse toEntity(PluralsightCourse source) {
+        return new ca.tsmoreland.courseinfo.domain.PluralsightCourse(
+            source.id(),
+            source.title(),
+            source.parseDuration().orElse(Duration.ZERO).toSeconds(),
+            PS_URL.formatted(source.contentUrl())
+        );
+    }
+}
